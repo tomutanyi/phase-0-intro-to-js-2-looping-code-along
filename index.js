85d3761c1bd333cb5658e81ac5f1c2fc8911bcee
tomutanyi <tomutanyi@gmail.com>
2023-07-14 01:06:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(event, eventName){
+    const names = []
+
+    for (let i = 0; i < event.length; i++) {
+        let name = (`Thank you, ${event[i]}, for the wonderful ${eventName} gift!`)
+        names.push(name)
+    }
+
+    return names
+
+}
+
+function countDown(numerals){
+    while (numerals >= 0) {
+        console.log(numerals)
+        numerals--
+    }
+}
+
+countDown(10)
---
+++ loop.js
@@ -0,0 +1,9 @@
+function countdown(number){
+    for (let i = 0; i < number.length; i++){
+        console.log(i)
+    }
+
+    return number
+}
+
+countdown(10)
